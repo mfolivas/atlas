d97a420af1ed9f1dd8dedbdcb699590680757bdd
Marcelo Olivas <Marcelo@Admins-MacBook-Pro.local>
2016-01-14 14:44:58 -0500

    Enable Hystrix via Spring Cloud.

--- src/main/java/com/mfolivas/atlas/AtlasApplication.java
+++ src/main/java/com/mfolivas/atlas/AtlasApplication.java
@@ -2,8 +2,10 @@ package com.mfolivas.atlas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class AtlasApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/mfolivas/atlas/ipinfo/IpInfoGeoLocationService.java
+++ src/main/java/com/mfolivas/atlas/ipinfo/IpInfoGeoLocationService.java
@@ -3,36 +3,48 @@ package com.mfolivas.atlas.ipinfo;
 import com.mfolivas.atlas.controller.GeoLocationResponse;
 import com.mfolivas.atlas.domain.IpRequest;
 import com.mfolivas.atlas.service.IpGeoLocationService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import javax.inject.Inject;
+import java.util.Random;
 
 /**
  * Implementation for the ipinfo.io.
  */
 @Service
-public class IpInfoGeoLocationService implements IpGeoLocationService {
+public class IpInfoGeoLocationService  {
 
     private final IpInfoConfiguration ipInfoConfiguration;
+    private static final String GEO_LOCATION_ONLY = "/{ip}/geo";
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    //temporary for testing purposes
+    private final Random random = new Random();
 
     @Inject
     public IpInfoGeoLocationService(IpInfoConfiguration ipInfoConfiguration) {
         this.ipInfoConfiguration = ipInfoConfiguration;
     }
 
-    @Override
+    @HystrixCommand(fallbackMethod = "defaultGeoName", groupKey = "GeoLocators", commandKey = "ipinfo.io")
     public GeoLocationResponse extractIpInformation(IpRequest ipRequest) throws Exception {
-        //To enable caching we need to enable context: https://github.com/Netflix/Hystrix/wiki/How-To-Use#request-context-setup
-        HystrixRequestContext context = HystrixRequestContext.initializeContext();
-        try {
-            IpInfoGeoLocationCommand ipInfoGeoLocationCommand = new IpInfoGeoLocationCommand(ipInfoConfiguration, ipRequest);
-            return ipInfoGeoLocationCommand.execute();
-        } finally {
-            context.shutdown();
-        }
+        //TODO(marcelo) need to figure out the caching aspect
+        Thread.sleep(random.nextInt(3) * 1000);
+        return restTemplate.getForObject(ipInfoConfiguration.getHost() + GEO_LOCATION_ONLY, GeoLocationResponse.class, ipRequest.getIp());
+    }
 
-
+    public GeoLocationResponse defaultGeoName(IpRequest ipRequest) {
+        GeoLocationResponse google = new GeoLocationResponse();
+        google.setIp("8.8.8.8");
+        google.setCity("Mountain View");
+        google.setRegion("California");
+        google.setCountry("US");
+        google.setLoc("37.3860,-122.0838");
+        google.setPostal("94040");
+        return google;
     }
 
 
