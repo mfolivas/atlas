9900a4e61d655c07fd121613b26c32687277d7ee
Marcelo Olivas <mfolivas@gmail.com>
2018-05-16 23:46:58 -0400

    Added new version of spring boot

--- src/main/java/com/mfolivas/atlas/atlas/AtlasApplication.java
+++ src/main/java/com/mfolivas/atlas/atlas/AtlasApplication.java
@@ -1,4 +1,4 @@
-package com.mfolivas.atlas;
+package com.mfolivas.atlas.atlas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AtlasApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AtlasApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasApplication.class, args);
+	}
 }
