ff693247fcfbd52929db18fc9784c2a56ed84a6d
Marcelo Olivas <mfolivas@gmail.com>
2018-05-17 13:04:30 -0400

    Added metrics

--- src/main/java/com/mfolivas/atlas/controller/IpGeoLocationController.java
+++ src/main/java/com/mfolivas/atlas/controller/IpGeoLocationController.java
@@ -3,12 +3,18 @@ package com.mfolivas.atlas.controller;
 import com.mfolivas.atlas.domain.InvalidIpAddress;
 import com.mfolivas.atlas.domain.IpRequest;
 import com.mfolivas.atlas.ipinfo.IpInfoGeoLocationService;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 
 /**
  * Controller to fetch the geo location from a particular IP by leveraging a third party provider.
@@ -18,14 +24,17 @@ public class IpGeoLocationController {
 
 
     private final IpInfoGeoLocationService ipInfoGeoLocationService;
+    private final Counter counter;
 
     @Inject
-    public IpGeoLocationController(IpInfoGeoLocationService ipInfoGeoLocationService) {
+    public IpGeoLocationController(IpInfoGeoLocationService ipInfoGeoLocationService, MeterRegistry meterRegistry) {
         this.ipInfoGeoLocationService = ipInfoGeoLocationService;
+        this.counter = meterRegistry.counter("received.ip");
     }
 
     @GetMapping("/{ip:.*}")
     public GeoLocationResponse extractGeoLocationByIp(@PathVariable String ip) throws Exception {
+        this.counter.increment();
         IpRequest ipRequest = IpRequest.valueOf(ip);
         return ipInfoGeoLocationService.extractIpInformation(ipRequest);
     }
--- src/main/java/com/mfolivas/atlas/ipinfo/IpInfoGeoLocationService.java
+++ src/main/java/com/mfolivas/atlas/ipinfo/IpInfoGeoLocationService.java
@@ -8,14 +8,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 
 /**
  * Implementation for the ipinfo.io.
  */
 @Service
-public class IpInfoGeoLocationService  {
+public class IpInfoGeoLocationService {
 
     private final IpInfoConfiguration ipInfoConfiguration;
     private static final String GEO_LOCATION_ONLY = "/{ip}/geo";
@@ -24,19 +29,31 @@ public class IpInfoGeoLocationService  {
     //temporary for testing purposes
     private final Random random = new Random();
 
+    private final Counter counter;
+    private final Timer timer;
+
     @Inject
-    public IpInfoGeoLocationService(IpInfoConfiguration ipInfoConfiguration) {
+    public IpInfoGeoLocationService(IpInfoConfiguration ipInfoConfiguration, MeterRegistry meterRegistry) {
         this.ipInfoConfiguration = ipInfoConfiguration;
+        this.counter = meterRegistry.counter("ipinfo.fallback");
+        this.timer = meterRegistry.timer("ipinfo.latency");
     }
 
     @HystrixCommand(fallbackMethod = "defaultGeoName", groupKey = "geoLocators", commandKey = "ipinfo.io")
     public GeoLocationResponse extractIpInformation(IpRequest ipRequest) throws Exception {
+        timer.record(() -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(2500);
+            } catch (InterruptedException ignored) {
+            }
+        });
         //TODO(marcelo) need to figure out the caching aspect
         Thread.sleep(random.nextInt(3) * 1000);
         return restTemplate.getForObject(ipInfoConfiguration.getHost() + GEO_LOCATION_ONLY, GeoLocationResponse.class, ipRequest.getIp());
     }
 
     public GeoLocationResponse defaultGeoName(IpRequest ipRequest) {
+        counter.increment();
         GeoLocationResponse google = new GeoLocationResponse();
         google.setIp("8.8.8.8");
         google.setCity("Mountain View");
